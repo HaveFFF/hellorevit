c87d4b40ca2aa929797b6e6d847e9c9b7d1f0273
ji <ji@007MHOME>
2016-09-24 23:54:59 +0800

    Hello Revit成功

---
+++ HelloRevit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+namespace HelloRevit
+{[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Class1:IExternalCommand
+    {public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,ref string message,ElementSet elements)
+        { TaskDialog.Show("Revit", "Hello Revit");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
