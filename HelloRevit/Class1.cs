2c6c7b6781fcb6ddb8a3969fcec173c0425b7c11
Dr.5 <Dr.5@DR5>
2016-09-24 13:55:27 +0800

    添加项目文件。

---
+++ HelloRevit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloRevit
+{
+    public class Class1
+    {
+    }
+}
